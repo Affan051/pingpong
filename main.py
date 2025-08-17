189271c64b33d68ad7dce185e1a27cd6d4f0cb68
Affan051 <haidaramaniaffan@gmail.com>
2025-08-17 11:00:39 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from pygame import *
+
+
+win_width = 600
+win_height = 500
+
+window = display.set_model((win_width, win_height))
+display.set_caption('ping pong mudah')
+
+background_color = (0, 0, 0)
+window.fill(background_color)
+
+run = True
+clock = time.Clock()
+fps = 60
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    
+    window.fill(background_color)
+    display.update()
+    clock.tick(fps)
