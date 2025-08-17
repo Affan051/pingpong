af688e171614b417f561cb8161cf91961fb8dce0
Affan051 <haidaramaniaffan@gmail.com>
2025-08-17 11:37:14 +0700

    Update  #1 main.py

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ display.set_caption('ping pong mudah')
 background_color = (0, 0, 0)
 window.fill(background_color)
 
+class GameSprite(sprite.Sprite):
+  # class constructor
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # We call the class constructor (Sprite):
+        sprite.Sprite.__init__(self)
+        # each sprite must store an image property
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
 run = True
 clock = time.Clock()
 fps = 60
